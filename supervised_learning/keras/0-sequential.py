f7edb5697d7aeb63e6cbd3eeb264de9c05a40f5c
Yamil Avila <yamilpra2@gmail.com>
2023-09-15 16:18:12 +0000

    tf runs fine

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""builds a neural network with the Keras library"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """neural network with the Keras library"""
+    model = K.Sequential()
+    L2 = K.regularizers.l2(lambtha)
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(layers[i], input_shape=(nx,),
+                                     activation=activations[i],
+                                     kernel_regularizer=L2,
+                                     name='dense'))
+        else:
+            model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=L2,
+                                     name='dense_' + str(i)))
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+    return model
