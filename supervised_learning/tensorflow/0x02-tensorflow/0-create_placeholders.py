c69b7a2aef08a073aca88bf15f3ce302e0abce4d
Yamil Avila <yamilpra2@gmail.com>
2023-10-07 22:15:59 +0000

    check attrib

---
+++ supervised_learning/tensorflow/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Placeholders"""
+
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """
+    function returns 2 placeholders x & y
+    nx: the number of feature columns in our data
+    classes: the number of classes in our classifier
+    """
+    return tf.placeholder(float, shape=[None, nx], name='x'), tf.placeholder(
+            float, shape=[None, classes], name='y')
